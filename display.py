aa89eed20e0fa46d6cb462fa1dfdb2d833a9b81e
Ziemowit Błażków <yasha2yashaa@gmail.com>
2018-09-10 17:37:51 +0200

    First option solved

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
+
 def menu():
     print(' What do you want to do?: ')
     print('1: View all imported albums')
@@ -6,3 +7,9 @@ def menu():
     print('4: Find the shortest or longest album')
     print('5: Find albums created by given artis')
     print('6: Find album by album')
+
+def view_all_imported_albums(albums_data):
+    albums_in_text=''
+    for album in albums_data:
+        albums_in_text+=str(album)+'\n'
+    return albums_in_text
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import display
 import inputs
 import music_reports
 
-display.menu()
-#inputs.menu_input()
-print(inputs.menu_input())
+def master_func():
+    tuple_of_functions=(display.view_all_imported_albums(file_handler.import_data()),1,2,3,4,5)
+    while True:
+        display.menu()
+        decision_made_by_the_user_of_this_program = inputs.menu_input()
+        for i in range(6):
+            if i ==  int(decision_made_by_the_user_of_this_program)-1:
+                print(tuple_of_functions[i]
+                break
+
+master_func()
